e105264da4e4373f7929304fc3ed4aff432795db
Deaton879 <Deaton879@live.com>
2022-05-12 22:09:58 -0400

    cms complete week04 documents/dropdown directive

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(
+      1, 
+      'CIT 260 - Object Oriented Programming', 
+      'Detailed description of Object Oriented Programming course goes here.', 
+      'https://content.byui.edu/file/b7c3e5ed-6947-497f-9d32-4e5b6b397cac/1/CIT 366 course description.pdf',
+      null
+    ),
+    new Document(
+      2, 
+      'CIT 366 - Full Web Stack Development', 
+      'Learn how to develop modern web applications using the MEAN stack', 
+      'https://content.byui.edu/file/b7c3e5ed-6947-497f-9d32-4e5b6b397cac/1/CIT 366 course description.pdf',
+      null
+    ),
+    new Document(
+      3, 
+      'CIT 425 - Data Warehousing', 
+      'Detailed description of Data Warehousing course goes here.', 
+      'https://content.byui.edu/file/b7c3e5ed-6947-497f-9d32-4e5b6b397cac/1/CIT 366 course description.pdf',
+      null
+    ),
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
